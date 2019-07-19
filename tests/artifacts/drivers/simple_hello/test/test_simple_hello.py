2f37c4b62fa65aa608c94916e593f1c0bf3ad97b
Jose Retelny <joseret@google.com>
2019-07-19 11:08:11 -0700

    testing build and then install and test

--- tests/artifacts/drivers/simple_hello/test/test_simple_hello.py
+++ tests/artifacts/drivers/simple_hello/test/test_simple_hello.py
@@ -46,6 +46,40 @@ class SimpleHelloTestCase(unittest.TestCase):
     self.assertTrue(len(response) >  0, "response empty - len == 0")
     self.assertIn("Last Call Successful", response, "Failed Test")
 
+  def test_simple_hello_uc3_with_google_com_token(self):
+
+    self.assertIn('GOOGLE_APPLICATION_CREDENTIALS', os.environ, "INGRESS_ISTIO_HOST environment variable not set")
+    self.assertTrue(len(os.environ['INGRESS_ISTIO_HOST']) >  0, "INGRESS_ISTIO_HOST empty - len == 0")
+    o_auth_helper = GCPAuthHelper()
+    token_gcf = o_auth_helper.get_google_open_id_connect_token("https://site2.ecom1.joecloudy.com")
+    full_url = self.get_url(os.environ['INGRESS_ISTIO_HOST'],
+                            'app-allowed-jwt-istio-appconfigv2-service-sm-', 'uc-allowed-jwt-istio', [ '2', '3' ])
+    headers = {"Host": "test-simple-hello.example.com"}
+    response = RestHelper(full_url).get_text(token_gcf,headers)
+    self.assertTrue(len(response) >  0, "response empty - len == 0")
+    self.assertIn("Last Call Successful", response, "Failed Test")
+
+  def util(self, parm_call, parm_svc, parm_ns):
+    full_url = "call" + parm_call + "=http://" + parm_svc
+    if parm_ns:
+      full_url = full_url + "." + parm_ns
+    full_url += "/testcallseq"
+    return full_url
+
+  def test_simple_hello_uc1_outbound_ok(self):
+    uc = "uc-allowed-services-k8s"
+    self.assertTrue(len(os.environ['INGRESS_NO_ISTIO_HOST']) >  0, "INGRESS_ISTIO_HOST empty - len == 0")
+    full_url = "http://" + os.environ['INGRESS_NO_ISTIO_HOST'] + "/testcallseq?"
+    full_url = full_url + self.util("1", "app-allowed-k8s-appconfigv2-service-sm-2", uc)
+    full_url = full_url + "&" + self.util("2", "app-allowed-k8s-appconfigv2-service-sm-1", uc)
+    full_url = full_url + "&call3=https://httpbin.org/get"
+
+    full_url = self.get_url(os.environ['INGRESS_ISTIO_HOST'],
+                            'app-allowed-jwt-istio-appconfigv2-service-sm-', 'uc-allowed-jwt-istio', [ '2', '3' ])
+    headers = {"Host": "test-simple-hello.example.com"}
+    response = RestHelper(full_url).get_text(None,headers)
+    self.assertTrue(len(response) >  0, "response empty - len == 0")
+    self.assertIn("Last Call Successful", response, "Failed Test")
 
 # if __name__ == '__main__':
 #   h = HtmlTestRunner.HTMLTestRunner(combine_reports=True, report_name="MyReport", add_timestamp=False).run(suite)
