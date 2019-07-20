2e7ef23eb0fb9cd36b1b0f3395d09f27c8a8056c
Jose Retelny <joseret@google.com>
2019-07-19 17:08:45 -0700

    testing build and then install and test

--- tests/artifacts/drivers/simple_hello/test/test_simple_hello.py
+++ tests/artifacts/drivers/simple_hello/test/test_simple_hello.py
@@ -31,20 +31,7 @@ class SimpleHelloTestCase(unittest.TestCase):
     headers = {"Host": "test-simple-hello.example.com"}
     response = RestHelper(full_url).get_text(token_gcf,headers)
     self.assertTrue(len(response) >  0, "response empty - len == 0")
-    self.assertIn("Last Call Successful", response, "Failed Test")
-
-  def test_simple_hello_uc3_with_google_com_token(self):
-
-    self.assertIn('GOOGLE_APPLICATION_CREDENTIALS', os.environ, "INGRESS_ISTIO_HOST environment variable not set")
-    self.assertTrue(len(os.environ['INGRESS_ISTIO_HOST']) >  0, "INGRESS_ISTIO_HOST empty - len == 0")
-    o_auth_helper = GCPAuthHelper()
-    token_gcf = o_auth_helper.get_google_open_id_connect_token("https://site2.ecom1.joecloudy.com")
-    full_url = self.get_url(os.environ['INGRESS_ISTIO_HOST'],
-                            'app-allowed-jwt-istio-appconfigv2-service-sm-', 'uc-allowed-jwt-istio', [ '2', '3' ])
-    headers = {"Host": "test-simple-hello.example.com"}
-    response = RestHelper(full_url).get_text(token_gcf,headers)
-    self.assertTrue(len(response) >  0, "response empty - len == 0")
-    self.assertIn("Last Call Successful", response, "Failed Test")
+    self.assertIn("401", response, "Failed Test")
 
   def test_simple_hello_uc3_with_google_com_token(self):
 
@@ -74,8 +61,6 @@ class SimpleHelloTestCase(unittest.TestCase):
     full_url = full_url + "&" + self.util("2", "app-allowed-k8s-appconfigv2-service-sm-1", uc)
     full_url = full_url + "&call3=https://httpbin.org/get"
 
-    full_url = self.get_url(os.environ['INGRESS_ISTIO_HOST'],
-                            'app-allowed-jwt-istio-appconfigv2-service-sm-', 'uc-allowed-jwt-istio', [ '2', '3' ])
     headers = {"Host": "test-simple-hello.example.com"}
     response = RestHelper(full_url).get_text(None,headers)
     self.assertTrue(len(response) >  0, "response empty - len == 0")
