87a6cf35cba5fbd82168fbe39b3bf42fbf9d69b7
bradley <cicenas@google.com>
2019-07-22 23:27:15 +0200

    Apply generated results from crd updates (#29)
    * Add gcpAccess descriptions, vaultInfo props to spec
    
    * apply generated results from crd updates

--- appconfigmgrv2/api/webhooks/builtins/pod_webhook.go
+++ appconfigmgrv2/api/webhooks/builtins/pod_webhook.go
@@ -11,11 +11,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
-// Copyright 2019 Google LLC. This software is provided as-is, 
+//
+// Copyright 2019 Google LLC. This software is provided as-is,
 // without warranty or representation for any use or purpose.
 //
-
 
 package builtins
 
