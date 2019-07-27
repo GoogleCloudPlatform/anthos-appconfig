74f073b277f315cbbe1dac05ab34f86845ce1ac3
Jose Retelny <joseret@google.com>
2019-07-27 15:29:54 -0700

    build fixes and constraint template goes in all not other

--- builder/kubebuilder-build/utils/acmsplit/main.go
+++ builder/kubebuilder-build/utils/acmsplit/main.go
@@ -121,7 +121,8 @@ func SplitYAMLDocuments(yamlBytes []byte) ([]byte, []byte, error) {
 		// Save the mapping between the gvk and the bytes that object consists of
 		switch typeMetaInfo.Kind {
 		case "Namespace":
-
+		case "ConstraintTemplate":
+			fallthrough
 		case "ClusterRole":
 			fallthrough
 		case "ClusterRoleBinding":
