20c575599ff89c6696643768c11d7efa177d91ad
Bradley Cicenas <cicenas@google.com>
2019-08-01 17:24:43 +0000

    remove unused imports

--- appconfigmgrv2/controllers/vault.go
+++ appconfigmgrv2/controllers/vault.go
@@ -4,11 +4,6 @@ import (
 	"context"
 	"fmt"
 
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/types"
-
-	"k8s.io/apimachinery/pkg/api/errors"
-
 	appconfigmgrv1alpha1 "github.com/GoogleCloudPlatform/anthos-appconfig/appconfigmgrv2/api/v1alpha1"
 )
 
