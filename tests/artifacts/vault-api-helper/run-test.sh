ff77f024cc425ff9a24f36a03278d92369a10fdd
Jose Retelny <joseret@google.com>
2019-08-11 22:13:11 -0700

    add token scopes

--- tests/artifacts/vault-api-helper/run-test.sh
+++ tests/artifacts/vault-api-helper/run-test.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright 2019 Google LLC. This software is provided as-is,
+# without warranty or representation for any use or purpose.#
+#
 
-#../../tests/artifacts/vault-api-helper/run-test.sh
 
 rm KC1;touch KC1;export KUBECONFIG=KC1; \
 gcloud container clusters get-credentials c-b-bcicen-uc-secrets-vault-13 --zone us-west1-b --project appcrd-cicenas-20190806
