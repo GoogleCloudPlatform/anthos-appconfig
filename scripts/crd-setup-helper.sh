25748261c40f26473e0f23f1e7952b7647c4f52c
Nick Stogner <stogner@google.com>
2019-08-26 15:05:56 -0700

    Pin gatekeeper release

--- scripts/crd-setup-helper.sh
+++ scripts/crd-setup-helper.sh
@@ -519,7 +519,7 @@ install_operator() {
 }
 
 install_gatekeeper() {
-  kubectl apply -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper/master/deploy/gatekeeper.yaml
+  kubectl apply -f https://raw.githubusercontent.com/open-policy-agent/gatekeeper/v3.0.4-beta.1/deploy/gatekeeper.yaml
   gsutil cat ${GATEKEEPER_BUCKET}/config.yaml | kubectl apply -f -
   sleep 10
   gsutil cat ${GATEKEEPER_BUCKET}/constraint-templates.yaml | kubectl apply -f -
